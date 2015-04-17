68274fc6c617dcb94013b1649239a311e948fa39
tacolin <potegrant@yahoo.com.tw>
2015-04-18 01:03:27 +0800

    add dtrace()

--- examples/oldevents_test/oldevents_test.c
+++ examples/oldevents_test/oldevents_test.c
@@ -4,8 +4,6 @@
 
 #include <signal.h>
 
-#define utrace() do { struct timeval _now = {}; gettimeofday(&_now, NULL); printf("(%3lds,%3ldms), tid = %ld, line %4d, %s()\n", _now.tv_sec % 1000, _now.tv_usec/1000, pthread_self(), __LINE__, __func__); } while(0)
-
 static tTask _taskNew;
 static tOevLoop _loopNew;
 
@@ -27,21 +25,21 @@ static void _runLoopNew(void* task, void* arg)
 
 static int _timer1Expired(tOevLoop* loop, void* ev, void* arg)
 {
-    utrace();
+    dtrace();
     dprint("_timer1Expired");
     return 0;
 }
 
 static int _timer2Expired(tOevLoop* loop, void* ev, void* arg)
 {
-    utrace();
+    dtrace();
     dprint("_timer2Expired");
     return 0;
 }
 
 static int _onceCallback(tOevLoop* loop, void* ev, void* arg)
 {
-    utrace();
+    dtrace();
     long i = (long)arg;
     dprint("i = %ld", i);
     return 0;
@@ -79,31 +77,31 @@ static int _processStdin(tOevLoop* loop, void* ev, void* arg)
     else if (strcmp(buf, "start timer1") == 0)
     {
         oevtm_init(&_timer1, _timer1Expired, 5000, NULL, OEV_TIMER_ONESHOT);
-        utrace();
+        dtrace();
         oevtm_start(&_loopNew, &_timer1);
     }
     else if (strcmp(buf, "stop timer1") == 0)
     {
-        utrace();
+        dtrace();
         oevtm_stop(&_loopNew, &_timer1);
     }
     else if (strcmp(buf, "start timer2") == 0)
     {
         oevtm_init(&_timer2, _timer2Expired, 2000, NULL, OEV_TIMER_PERIODIC);
-        utrace();
+        dtrace();
         oevtm_start(&_loopNew, &_timer2);
     }
     else if (strcmp(buf, "stop timer2") == 0)
     {
-        utrace();
+        dtrace();
         oevtm_stop(&_loopNew, &_timer2);
     }
     else if (strcmp(buf, "evonce") == 0)
     {
-        int i = 0;
+        long i = 0;
         for (i=0; i<10; i++)
         {
-            utrace();
+            dtrace();
             oev_once(&_loopNew, _onceCallback, (void*)i);
         }
     }
@@ -122,7 +120,7 @@ int main(int argc, char const *argv[])
 
     task_system_init();
 
-    utrace();
+    dtrace();
 
     oevloop_init(&loop);
 
