6fbacf89d00accebffc5fe0d35732ecd5a88bfb3
tacolin <potegrant@yahoo.com.tw>
2015-05-19 10:49:00 +0800

    add bits_put_le and bits_get_le

--- source/pack.c
+++ source/pack.c
@@ -119,6 +119,100 @@ void bits_put(void* bytes, int* ofs, int bit_num, unsigned int value)
     return;
 }
 
+static unsigned char _get_bits_le(unsigned char* bytes, int ofs, int bit_num)
+{
+    unsigned char ret = bytes[bit_address(ofs)];
+
+    CHECK_IF(bytes == NULL, return 0, "bytes is null");
+    CHECK_IF((bit_offset(ofs)+bit_num) > 8, return 0, "get more than 8 bits");
+
+    ret >>= bit_offset(ofs);
+    ret  &= (0xFF >> (8 - bit_num));
+    return ret;
+}
+
+static void _put_bits_le(unsigned char* bytes, int ofs, int bit_num, unsigned char value)
+{
+    unsigned char mask = 0xFF << (8 - bit_num);
+    mask = mask >> (8 - bit_offset(ofs) - bit_num);
+
+    CHECK_IF(bytes == NULL, return, "bytes is null");
+    CHECK_IF((bit_offset(ofs)+bit_num) > 8, return, "put more than 8 bits");
+
+    bytes    += bit_address(ofs);
+    value   <<= bit_offset(ofs);
+    bytes[0] &= ~mask;
+    bytes[0] |= (value & mask);
+}
+
+unsigned int bits_get_le(void* bytes, int ofs, int bit_num)
+{
+    unsigned char* start = ((unsigned char*)bytes) + bit_address(ofs);
+    unsigned char* stop  = ((unsigned char*)bytes) + bit_address(ofs+bit_num);
+    unsigned char  bits = 0;
+
+    unsigned int tmp    = 0;
+    unsigned int ret    = 0;
+
+    CHECK_IF(bytes == NULL, return 0, "bytes is null");
+    CHECK_IF(bit_num <= 0, return 0, "bit_num = %d invalid", bit_num);
+    CHECK_IF(bit_num > 32, return 0, "get more than 32 bits");
+
+    if ((bit_offset(ofs) + bit_num) <= 8)
+    {
+        ret = _get_bits_le((unsigned char*)bytes, ofs, bit_num);
+    }
+    else if ((bit_offset(ofs) + bit_num) < 40)
+    {
+        tmp  = _get_bits_le(start, bit_offset(ofs), (8 - bit_offset(ofs)));
+        ret |= tmp;
+
+        while (start++ < (stop - 1))
+        {
+            tmp      = *start << (8 - bit_offset(ofs) + bits);
+            ret     |= tmp;
+            bits  += 8;
+        }
+
+        tmp  = _get_bits_le(stop, 0, bit_offset(ofs+bit_num)) << (8 - bit_offset(ofs) + bits);
+        ret |= tmp;
+    }
+    return ret;
+}
+
+void bits_put_le(void* bytes, int *ofs, int bit_num, unsigned int value)
+{
+    unsigned char* start = ((unsigned char*)bytes) + bit_address(*ofs);
+    unsigned char* stop  = ((unsigned char*)bytes) + bit_address(*ofs+bit_num);
+    unsigned char  bits = 0;
+
+    CHECK_IF(bytes == NULL, return, "bytes is null");
+    CHECK_IF(ofs == NULL, return, "ofs is null");
+    CHECK_IF(bit_num <= 0, return, "bit_num = %d invalid", bit_num);
+    CHECK_IF(bit_num > 32, return, "bit_num = %d invalid", bit_num);
+
+    value = value & (0xFFFFFFFF >> (32 - bit_num));
+
+    if ((bit_offset(*ofs) + bit_num) <= 8)
+    {
+        _put_bits_le((unsigned char*)bytes, *ofs, bit_num, (unsigned char)value);
+    }
+    else
+    {
+        _put_bits_le(start, bit_offset(*ofs), (8 - bit_offset(*ofs)), value);
+
+        while (start++ < (stop - 1))
+        {
+            *start = (value >> (8 - bit_offset(*ofs) + bits)) & 0xFF;
+            bits += 8;
+        }
+
+        _put_bits_le(stop, 0, bit_offset(*ofs+bit_num), 0xFF & (value >> (8 - bit_offset(*ofs) + bits)));
+    }
+    *ofs += bit_num;
+    return;
+}
+
 int bitmap_to_str(unsigned int bitmap, int bit_num, char* str_buf, int buf_size)
 {
     CHECK_IF(str_buf == NULL, return -1, "str_buf is null");
